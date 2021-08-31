a10578d8a27c71db1ef8a9b1257d1f7d3ddc7a08
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-30 23:02:13 -0600

    added connection to database

--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 const inquirer = require("inquire")
 const mysql = require("mysql")
 const CTable = require('console.table')
+require("dotenv").config();  //https://www.npmjs.com/package/dotenv
+
+//connection to MySQL        //https://www.tabnine.com/code/javascript/functions/mysql/createConnection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    password: process.env.DB_PASS,
+    database: "employee_tracker"
+});
+
