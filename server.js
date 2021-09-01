0a7e86c664d22ffe446536a9cfb90883fe00cdf6
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 21:30:06 -0600

    removed server connection

--- server.js
+++ server.js
@@ -8,11 +8,3 @@ require("dotenv").config();  //https://www.npmjs.com/package/dotenv
 //port to live server
 const PORT = process.env.PORT || 3000;
 
-//connection to MySQL        //https://www.tabnine.com/code/javascript/functions/mysql/createConnection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    password: process.env.DB_PASS,
-    database: "employee-tracker"
-});
-
