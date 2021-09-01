0a4736cf1ec59957fa03a6251a7ce7ecfa568033
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 21:30:21 -0600

    added server conenction

--- config/connections.js
+++ config/connections.js
@@ -3,9 +3,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 //connection to server
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({  ////https://www.tabnine.com/code/javascript/functions/mysql/createConnection
     host: "localhost",
     port: 3306,
+    user: 'root',
     password: process.env.DB_PASS,
     database: "employee-tracker"
 });
