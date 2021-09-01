3ab7f00484ccb642a03a000f730252bbb911231d
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 21:39:40 -0600

    figlet dependency added

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const inquirer = require("inquire")
 const mysql = require("mysql")
 const CTable = require('console.table')
 const chalk = require('chalk')  //https://www.npmjs.com/package/chalk
+const figlet = require('figlet')  //https://www.npmjs.com/package/figlet
 require("dotenv").config();  //https://www.npmjs.com/package/dotenv
 
 //port to live server
 const PORT = process.env.PORT || 3000;
 
+//connection to database and figlet title
