b39caa62a0dc3ecf538a650e93cdfca3e61e232a
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 23:31:52 -0600

    first prompt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const CTable = require('console.table')
 const chalk = require('chalk')  //https://www.npmjs.com/package/chalk
 const figlet = require('figlet')  //https://www.npmjs.com/package/figlet
 require("dotenv").config();  //https://www.npmjs.com/package/dotenv
+const connection = require('./config/connections');
 
 //port to live server
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,88 @@ if (error) throw error;
 console.log(chalk.orange.bold('==================================================================='));
 console.log('');
 console.log(chalk.greenBright.bold(figlet.textSync('Employee Tracker')));
-console.log(``);
+console.log('');
     console.log(chalk.orange.bold(`===================================================================`));
-    userPrompt();
+    choicesPrompt();
 });
+
+//choices prompt
+const choicesPrompt = () => {
+    inquirer.prompt([
+        {
+            type : "list",
+            message: "Please select an action below",
+            name: "choice",
+            choices: [
+                "View All Employees?", 
+                "View All Employee's By Roles?",
+                "View all Employees By Departments", 
+                "Update Employee",
+                "Add Employee?",
+                "Add Role?",
+                "Add Department?"
+            ]
+        }
+    ])
+
+    //
+    .then((answers) => {
+        const {choices} = answers;
+  
+          if (choices === 'View All Employees') {
+              viewAllEmployees();
+          }
+  
+          if (choices === 'View All Departments') {
+            viewAllDepartments();
+        }
+  
+          if (choices === 'View All Employees By Department') {
+              viewEmployeesByDepartment();
+          }
+  
+          if (choices === 'Add Employee') {
+              addEmployee();
+          }
+  
+          if (choices === 'Remove Employee') {
+              removeEmployee();
+          }
+  
+          if (choices === 'Update Employee Role') {
+              updateEmployeeRole();
+          }
+  
+          if (choices === 'Update Employee Manager') {
+              updateEmployeeManager();
+          }
+  
+          if (choices === 'View All Roles') {
+              viewAllRoles();
+          }
+  
+          if (choices === 'Add Role') {
+              addRole();
+          }
+  
+          if (choices === 'Remove Role') {
+              removeRole();
+          }
+  
+          if (choices === 'Add Department') {
+              addDepartment();
+          }
+  
+          if (choices === 'View Department Budgets') {
+              viewDepartmentBudget();
+          }
+  
+          if (choices === 'Remove Department') {
+              removeDepartment();
+          }
+  
+          if (choices === 'Exit') {
+              connection.end();
+          }
+    });
+  };
