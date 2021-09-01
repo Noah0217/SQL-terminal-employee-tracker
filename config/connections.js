8282b17075d8e78710c2a87d43da4176c1053e3e
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 21:13:43 -0600

    dependencies and server conection added

--- config/connections.js
+++ config/connections.js
@@ -1,1 +1,13 @@
-//
+//dependencies
+const mysql = require('mysql2');
+require('dotenv').config();
+
+//connection to server
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    password: process.env.DB_PASS,
+    database: "employee-tracker"
+});
+
+module.export = connection;
