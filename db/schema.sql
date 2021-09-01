bb56e4ce2287ed422fba16e1d71686ab5f196532
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 20:38:28 -0600

    added role, department, employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
---
+--deleting data base if already exist and creating a new one
+DROP DATABASE IF EXIST employee-tracker;
+CREATE DATABASE employee-tracker;
+USE employee-tracker;  --https://dev.mysql.com/doc/refman/8.0/en/use.html
+
+--department table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+
+  --role table
+  CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+--employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,44 @@
---
+--seeds department
+INSERT INTO department (name)
+VALUE ("Sales");
+
+INSERT INTO department (name)
+VALUE ("Finance");
+
+INSERT INTO department (name)
+VALUE ("Marketing");
+
+INSERT INTO department (name)
+VALUE ("Engineering");
+
+INSERT INTO department (name)
+VALUE ("Legal");
+
+--seeds role
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 85000, 1);
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Senior Engineer", 125000, 1);
+
+INSERT INTO role(title, salary, department_id)
+VALUES("CFO", 350000, 3);
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Chief Counsel", 300000, 4);
+
+--seeds employee
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Johnnie', 'Random', 1, 2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('James', 'Smith', 1, null);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Ronnie', 'Manning', 1, 2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Jimmy', 'Jones', 2, 2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Larry', 'Legal', 4, null);
