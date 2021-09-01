2a3696cde6bf6d9cb71121046100349d57a348f5
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 22:02:46 -0600

    finished server figlet title

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //dependencies
-const inquirer = require("inquire")
-const mysql = require("mysql")
+const inquirer = require("inquirer")
+const mysql = require("mysql2")
 const CTable = require('console.table')
 const chalk = require('chalk')  //https://www.npmjs.com/package/chalk
 const figlet = require('figlet')  //https://www.npmjs.com/package/figlet
@@ -10,3 +10,12 @@ require("dotenv").config();  //https://www.npmjs.com/package/dotenv
 const PORT = process.env.PORT || 3000;
 
 //connection to database and figlet title
+connection.connect((error) => {
+if (error) throw error;
+console.log(chalk.orange.bold('==================================================================='));
+console.log('');
+console.log(chalk.greenBright.bold(figlet.textSync('Employee Tracker')));
+console.log(``);
+    console.log(chalk.orange.bold(`===================================================================`));
+    userPrompt();
+});
