ab955687d7ac20e290667036ac8894b4ae44fab1
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-31 18:41:27 -0600

    added port to live server

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const mysql = require("mysql")
 const CTable = require('console.table')
 require("dotenv").config();  //https://www.npmjs.com/package/dotenv
 
+//port to live server
+const PORT = process.env.PORT || 3000;
+
 //connection to MySQL        //https://www.tabnine.com/code/javascript/functions/mysql/createConnection
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     password: process.env.DB_PASS,
-    database: "employee_tracker"
+    database: "employee-tracker"
 });
 
