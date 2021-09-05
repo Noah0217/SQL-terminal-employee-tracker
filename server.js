022ac4abc846eb79f8f12c94e18322a113c75548
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-04 20:25:36 -0600

    fixed minor errors still some to work on

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ function viewEmployeesByDepartment() {
 
   //update employee
   function updateEmployee() {
-    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, results) {
      if (err) throw err
      console.log(results)
     inquirer.prompt([
@@ -205,4 +205,32 @@ function viewEmployeesByDepartment() {
       })
 
   })
+}
+
+
+
+var roleArr = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
+
+  })
+  return roleArr;
+}
+
+
+
+var managersArr = [];
+function selectManager() {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managersArr.push(res[i].first_name);
+    }
+
+  })
+  return managersArr;
 }
